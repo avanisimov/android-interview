724c4e3b15b8d66112c19fd3bcc671595ca17218
Aleksei Anisimov <suxelshock@gmail.com>
2022-07-12 10:29:03 +0300

    Init commit

---
+++ src/main/kotlin/tech/constanta/android/interview/coroutines/CoroutinesTask1.kt
@@ -0,0 +1,34 @@
+package tech.constanta.android.interview.coroutines
+
+import kotlinx.coroutines.runBlocking
+
+/**
+ * Дописать функцию getNetworkData так, чтобы он вернул результат NetworkDataGetter::getData
+ */
+class NetworkDataGetter {
+
+    fun getData(callback: Callback) {
+        println("Getting network data...")
+        Thread.sleep(1000)
+        println("Got network data...")
+        callback.onDataGot("Some data")
+    }
+
+    interface Callback {
+
+        fun onDataGot(data: String)
+    }
+}
+
+suspend fun getNetworkData(): String {
+    val networkDataGetter = NetworkDataGetter()
+    TODO()
+}
+
+fun main() {
+    runBlocking {
+        val getNetworkDataResult = getNetworkData()
+        println(getNetworkDataResult)
+    }
+}
+
